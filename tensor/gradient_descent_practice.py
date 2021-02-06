3bd7ba61cf513b02efed6176bf57527ea0f14f65
Caumannerman <uigohiiming2@gmail.com>
2021-02-06 19:20:39 +0900

    Basic_of_Gradient_descent

---
+++ tensor/gradient_descent_practice.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+
+
+x_data = [1, 2, 3, 4, 5]
+y_data = [1, 2, 3, 4, 5]
+
+W = tf.Variable(5.6)
+b = tf.Variable(0.5)
+
+learning_rate = 0.01
+
+for i in range(1000):
+    # with 문
+    with tf.GradientTape() as tape:
+        hypothesis = W * x_data + b
+        cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+
+    W_grad, b_grad = tape.gradient(cost, [W, b])
+    W.assign_sub(learning_rate * W_grad)
+    b.assign_sub(learning_rate * b_grad)
+    if i % 10 == 0:
+        print("{:5}|{:10.4f}|{:10.4}|{:10.6f}".format(i, W.numpy(), b.numpy(), cost))
+
+#확인
+print( W*5 + b)
